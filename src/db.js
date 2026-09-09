d9fa777d94519fe695abefd3f3328b8efc0d7313
owfran <owfran.2022@gmail.com>
2026-09-09 10:23:22 -0400

    try fix database path

--- src/db.js
+++ src/db.js
@@ -2,8 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const { DatabaseSync } = require("node:sqlite");
 
-const dbPath = process.env.DATABASE_PATH || path.join(__dirname, "..", "data", "bluetag.db");
+//const dbPath = process.env.DATABASE_PATH || path.join(__dirname, "..", "data", "bluetag.db");
+//fs.mkdirSync(path.dirname(dbPath), { recursive: true });
+const dbPath = path.join('/tmp', 'bluetag.db');
 fs.mkdirSync(path.dirname(dbPath), { recursive: true });
+
 
 const db = new DatabaseSync(dbPath);
 db.exec("PRAGMA journal_mode = WAL");
